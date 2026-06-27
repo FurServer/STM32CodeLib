15efd6ee1e3e62e9f5537b854cf2227b2040d108
Async异步泡 <furserver@qq.com>
2026-06-27 14:44:32 +0800

    添加u8g2示例

---
+++ u8g2_stm32/example.c
@@ -0,0 +1,22 @@
+#include "u8g2.h"
+#include "u8g2_stm32.h"
+
+// 设备句柄
+u8g2_t u8g2;
+
+int main(void)
+{
+	// 初始化
+	u8g2Init(&u8g2);
+	
+	while(1)
+    {
+		// 绘制
+		u8g2_FirstPage(&u8g2);
+        do
+        {
+            draw_u8g2_example(&u8g2);
+        }
+        while (u8g2_NextPage(&u8g2));
+    }
+}
